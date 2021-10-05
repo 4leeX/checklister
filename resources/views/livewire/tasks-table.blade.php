e9ee644bd8c9c4355d2f9083f97ccf37ac756a2a
Alexsandro <alexsandro.sousa12366@gmail.com>
2021-10-05 20:56:03 -0300

    fix tasks -livewire

--- resources/views/livewire/tasks-table.blade.php
+++ resources/views/livewire/tasks-table.blade.php
@@ -1,8 +1,20 @@
-<table class="table table-responsive-sm table-striped" wire:sortable="updateTaskOrder">
+<table class="table table-responsive-sm table-striped">
                                 
     <tbody>
         @foreach ($tasks as $task)
-        <tr wire:sortable.item="{{ $task->id }}" wire:key="task-{{ $task->id }}">
+        <tr>
+            <td>
+                @if ($task->position > 1)
+                <a wire:click.prevent="task_up({{ $task->id }})" href="#">
+                    &#x21e7;
+                </a>
+                @endif
+                @if ($task->position < $tasks->max('position'))
+                <a wire:click.prevent="task_down({{ $task->id }})" href="#">
+                    &#x21e9;
+                </a>
+                @endif
+            </td>
             <td>{{ $task->name }}</td>
             <td>
                 <a class="btn btn-sm btn-primary"
